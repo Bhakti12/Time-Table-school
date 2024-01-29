c0c0a1d35a5e87b94112bd79773c43922267b343
Bhakti12 <sanghanibhakti922@gmail.com>
2024-01-29 18:56:33 +0530

    remove unnecessary code and update readme.md

--- structure.js
+++ structure.js
@@ -12,31 +12,26 @@ function createSubject(time, subject, teacher) {
   
   const schedule = [];
   
-  // Loop through each division
   divisions.forEach((division) => {
     const divisionSchedule = [];
   
-    // Loop through each day of the week
     daysOfWeek.forEach((day) => {
       const daySchedule = {
-        date: '2024/6/1', // You might want to dynamically generate dates
+        date: '2024/6/1',
         day,
         subjects: [],
       };
   
-      // Add subjects to the day's schedule
       daySchedule.subjects.push(
         createSubject('7:00 to 8:00 AM', 'Maths', 'Mr. Pole'),
         createSubject('7:00 to 8:00 AM', 'English', 'Mrs. Shah'),
         createSubject('7:00 to 8:00 AM', 'Science', 'Mrs. Acharya'),
         createSubject('7:00 to 8:00 AM', 'Computer Basics', 'Mr. Sarabhai')
-        // Add more subjects as needed
       );
   
       divisionSchedule.push(daySchedule);
     });
   
-    // Add the division's schedule to the overall schedule
     schedule.push({ [division]: divisionSchedule });
   });
   
